088dcf7bbde79e52a8f99861df067786547a2a86
rxt30 <mhampel@pm.me>
2021-07-14 13:32:39 +0200

    Changed wallpaper

--- .config/awesome/themes/powerarrow-dark/theme.lua
+++ .config/awesome/themes/powerarrow-dark/theme.lua
@@ -15,7 +15,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/powerarrow-dark"
-theme.wallpaper_self                            = theme.dir .. "/bg.jpg"
+theme.wallpaper_self                            = theme.dir .. "/wall.jpg"
 theme.font                                      = "Roboto 9"
 theme.fg_normal                                 = "#FFFFFF"
 theme.fg_focus                                  = "#EA6F81"
