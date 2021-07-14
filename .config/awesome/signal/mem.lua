0cf5de45de8d00822842d15fc6d26f33d0273318
rxt30 <mhampel@pm.me>
2021-07-14 09:37:01 +0200

    Added signal for memory-usage

---
+++ .config/awesome/signal/mem.lua
@@ -0,0 +1,21 @@
+local awful = require('awful')
+local gears = require('gears')
+
+local getMemUsage = function ()
+   return awful.spawn.easy_async_with_shell(
+        "free -m | awk 'FNR==2{print $3}'"
+        function (stdout)
+            memUsage = tonumber(stdout)
+            awesome.emit_signal("signal::mem", memUsage)
+        end
+       )
+end
+
+local mem_timer = gears.timer {
+    timeout = 10,
+    autostart = true,
+    call_now = true,
+    callback = getMemUsage
+}
+
+return mem_timer
