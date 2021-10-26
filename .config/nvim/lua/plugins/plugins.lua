e6868b8bb248d3b68dcc9691aefb9f2bd8943265
rxt30 <mhampel@pm.me>
2021-10-26 11:07:59 +0200

    Added file explorer for nvim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -83,6 +83,8 @@ return packer.startup(function ()
 
     use {
         "kyazdani42/nvim-tree.lua",
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function() require'nvim-tree'.setup{} end,
         cmd = { "NvimTreeToggle" }
     }
 
