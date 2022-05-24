b181434cd3fc184ad8f166aca697c0cb0c9d2202
Max H <36088096+rxt30@users.noreply.github.com>
2022-05-24 13:03:38 +0200

    added comand to convert to hex

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,3 +34,4 @@ end
 
 alias cht="cht.sh"
 alias discord="discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy"
+alias hex="math --base=hex"
