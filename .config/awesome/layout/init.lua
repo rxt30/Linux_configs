4db6394be322a3efe49bf9dec0077c60be8ad12f
rxt30 <mhampel@pm.me>
2022-06-18 12:11:10 +0200

    Cleanup

--- .config/awesome/layout/init.lua
+++ .config/awesome/layout/init.lua
@@ -2,9 +2,7 @@ local awful = require('awful')
 local beautiful = require('beautiful')
 local gears = require('gears')
 local top_panel = require('layout.top_bar')
-local left_panel = require('layout.left_bar')
 local tag_creation = require('layout.tags')
-local theme = require('themes.powerarrow-dark.theme')
 
 
 beautiful.wallpaper = gears.filesystem.get_configuration_dir() .. "/theme/wallpapers/bg.jpg"
@@ -38,10 +36,6 @@ awful.screen.connect_for_each_screen(function(s)
 
         tag_creation.create_tags(s)
         top_panel(s)
-        if s == screen.primary then
-            left_panel(s)
-        end
---        theme.at_screen_connect(s)
 
     end
 )
