4ebf5a0e4367d20571264b7b248fcf4b05dfc91e
rxt30 <mhampel@pm.me>
2022-06-22 09:11:34 +0200

    Added formatter configuration for stylua

--- .config/nvim/lua/plugins/configs/formatter.lua
+++ .config/nvim/lua/plugins/configs/formatter.lua
@@ -1,10 +1,26 @@
-local formatter = require("formatter")
+local formatter = require('formatter')
+local util = require("formatter.util")
 
 formatter.setup({
   filetype = {
     go = {
       require("formatter.filetypes.go").gofmt,
     },
+    lua = {
+      function()
+        return {
+          exe = "stylua",
+          args = {
+            "--indent-type Spaces",
+            "--indent-width 2",
+            "--quote-style ForceDouble",
+            "--stdin-filepath",
+            util.escape_path(util.get_current_buffer_file_path())
+          },
+          stdin = true
+        }
+      end
+    }
   },
 })
 
