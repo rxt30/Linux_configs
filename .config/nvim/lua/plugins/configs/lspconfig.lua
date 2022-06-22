2c7161985739b09616f950e36364e3d2c51b594f
rxt30 <mhampel@pm.me>
2022-06-22 08:39:35 +0200

    Enabled autocomplete support for ltex

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspinstaller = require("nvim-lsp-installer")
 lspinstaller.setup({})
 local all_servers = lspinstaller.get_installed_servers()
 for _, lsp in ipairs(all_servers) do
-  lspconfig[lsp.name].setup(coq.lsp_ensure_capabilities({}))
+ lspconfig[lsp.name].setup(coq.lsp_ensure_capabilities({}))
 end
 
 -- Custom configurations
@@ -13,6 +13,7 @@ lspconfig.ltex.setup(coq.lsp_ensure_capabilities({
   on_attach = on_attach,
   settings = {
     ltex = {
+      completionEnabled = true,
       language = "de",
     },
   },
