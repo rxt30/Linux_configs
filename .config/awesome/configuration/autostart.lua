f8d143c479f5c81556ae6e065dab7b3a7ac16ba9
rxt30 <mhampel@pm.me>
2022-08-20 15:29:47 +0200

    Removed picom, as it may not needed

--- .config/awesome/configuration/autostart.lua
+++ .config/awesome/configuration/autostart.lua
@@ -6,11 +6,13 @@ local helpers = require("helpers")
 local function autostart_apps()
   --- Lockscreen & Display configuration
   awful.spawn.easy_async_with_shell("xlayoutdisplay")
-  awful.spawn.easy_async_with_shell("XSECURELOCK_PASSWORD_PROMPT=kaomoji XSECURELOCK_BLANK_TIMEOUT=5 XSECURELOCK_BLANK_DPMS_STATE=off xss-lock -l -- xsecurelock")
+  awful.spawn.easy_async_with_shell(
+    "XSECURELOCK_PASSWORD_PROMPT=kaomoji XSECURELOCK_BLANK_TIMEOUT=5 XSECURELOCK_BLANK_DPMS_STATE=off xss-lock -l -- xsecurelock"
+  )
   --- Compositor
-  helpers.run.check_if_running("picom --experimental-backends", nil, function()
-    awful.spawn("picom --experimental-backends --config " .. config_dir .. "configuration/picom.conf", false)
-  end)
+  -- helpers.run.check_if_running("picom --experimental-backends", nil, function()
+  -- awful.spawn("picom --experimental-backends --config " .. config_dir .. "configuration/picom.conf", false)
+  -- end)
   --- Music Server
   helpers.run.run_once_pgrep("mpd")
   helpers.run.run_once_pgrep("mpDris2")
