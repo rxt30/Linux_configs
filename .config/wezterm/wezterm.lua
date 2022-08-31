0e8bfc2052f8ab33d1b3e7207ca27dd858776f18
rxt30 <mhampel@pm.me>
2022-08-31 12:15:45 +0200

    Added custom padding for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,4 +5,10 @@ return {
   font = wezterm.font("Noto Sans Mono", { weight = "Regular" }),
   font_size = 10,
   enable_tab_bar = false,
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 2,
+    bottom = 2,
+  },
 }
