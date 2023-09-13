26ca1ed96f3d67f7fd22d8f73270f380d8999217
--global <mhampel@pm.me>
2023-09-13 23:43:00 +0200

    Further packages for emacs

--- .config/emacs/packages.el
+++ .config/emacs/packages.el
@@ -2,56 +2,15 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-(require 'evil)
-(evil-mode 1)
+(load-file "~/.config/emacs/packages/evil.el")
+(load-file "~/.config/emacs/packages/auto-completion.el")
+(load-file "~/.config/emacs/packages/lsp.el")
+(load-file "~/.config/emacs/packages/catppuccin.el")
+(load-file "~/.config/emacs/packages/format-all.el")
+(load-file "~/.config/emacs/packages/indent.el")
 
-;; Auto-completion
-(unless (package-installed-p 'company)
-  (package-install 'company))
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-
-
-(defun select-next-or-indent ()
-  (interactive)
-  (if (company-manual-begin)
-      (company-select-next-if-tooltip-visible-or-complete-selection)
-    (indent-according-to-mode)))
-
-(defun select-previous-or-indent ()
-  (interactive)
-  (if (company-manual-begin)
-      (company-select-previous-or-abort)
-    (indent-according-to-mode)))
-
-(eval-after-load 'company
-  '(progn
-     (define-key company-active-map (kbd "TAB") #'select-next-or-indent)
-     (define-key company-active-map [tab] #'select-next-or-indent)
-     (define-key company-active-map (kbd "BACKTAB") #'select-previous-or-indent)
-     (define-key company-active-map [backtab] #'select-previous-or-indent)))
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
-(setq company-selection-wrap-around t)
-
-;; LSP
-(unless (package-installed-p 'lsp-mode)
-  (package-install 'lsp-mode))
-;; LSP UI
-(unless (package-installed-p 'lsp-ui)
-  (package-install 'lsp-ui))
-
-;; Python
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-mode . (lambda ()
-			 (require 'lsp-pyright)
-			 (lsp))))
-
-;; Catppuccin
-(unless (package-installed-p 'catppuccin-theme)
-  (package-install 'catppuccin-theme))
-(load-theme 'catppuccin :no-confirm)
+;; All the icons
+(unless (package-installed-p 'all-the-icons)
+  (package-install 'all-the-icons))
+(when (display-graphic-p)
+  (require 'all-the-icons))
--- .emacs
+++ .emacs
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight-indent-guides-method 'bitmap)
  '(package-selected-packages '(company evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
