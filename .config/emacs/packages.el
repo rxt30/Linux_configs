96f8b25d93c8281320f2c3792fc5d501288e6088
--global <mhampel@pm.me>
2023-09-14 10:19:35 +0200

    Added rainbowbrackets

--- .config/emacs/packages.el
+++ .config/emacs/packages.el
@@ -14,3 +14,8 @@
   (package-install 'all-the-icons))
 (when (display-graphic-p)
   (require 'all-the-icons))
+
+;;Rainbow brackets
+(unless (package-installed-p 'rainbow-delimiters)
+  (package-install 'rainbow-delimiters))
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
